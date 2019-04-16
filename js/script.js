d0602e108ad28bc415432668270916875aca2143
James <ioandumitru59@gmail.com>
2019-04-16 00:06:02 -0500

    Updated index.html & js.script
    Added my name and iit email to the page. Added pop-up and a comment as well.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 function init(){
 //add your javascrip between these two lines of code
 
+  var text = document.getElementById("entryinput");
+  var button = document.getElementById("entrybutton");
+
+  /*Will alert the user with a popup message with whatever was written.*/
+  button.addEventListener("click", function () {
+    alert("James Dumitru: " + text.value);
+    document.getElementById("textoutput").innerHTML = text.value
+	});
 
 
-
-
-
-
-
+}
 window.addEventListener('load', init);
